2bccf3fbf6aa918700a765e06a89a528ee42456d
Michael Retchin <michael.retchin@choderalab.org>
2023-07-27 11:18:08 -0400

    New environment that can perform surrogate measurements AND library designs, in a single unified action space.

--- dgym/env/drug_env.py
+++ dgym/env/drug_env.py
@@ -3,111 +3,89 @@ from typing import Iterable
 
 import gymnasium as gym
 import numpy as np
-from gymnasium import spaces
+from gymnasium.spaces import (
+    Dict, Discrete, Box, Sequence, Tuple
+)
 
 
 class DrugEnv(gym.Env):
-    """Custom Environment that follows gym interface."""
-
-    metadata = {"render_modes": ["human"], "render_fps": 30}
-
-    def __init__(self, arg1, arg2, ...):
+    def __init__(self):
         super().__init__()
-        # Define action and observation space
-        # They must be gym.spaces objects
-        # Example when using discrete actions:
-        self.action_space = spaces.Discrete(N_DISCRETE_ACTIONS)
-        # Example for using image as input (channel-first; channel-last also works):
-        self.observation_space = spaces.Box(low=0, high=255,
-                                            shape=(N_CHANNELS, HEIGHT, WIDTH), dtype=np.uint8)
+
+        # Define the maximum number of molecules that could ever be synthesized
+        self.max_molecules = 10_000
+
+        # Define the action space
+        self.action_space = Dict({
+            'design': Dict({
+                'num_analogs': Discrete(self.max_molecules),
+                'percent_random': Box(low=0.0, high=1.0, shape=(1,))
+            }),
+            'order': Dict({
+                'assay': Discrete(3),  # For example, if you have 3 assays
+                'molecule': Discrete(self.max_molecules)
+            })
+        })
+
+        # Define the observation space
+        self.observation_space = Dict({
+            'library': Box(low=-float('inf'), high=float('inf'), shape=(self.max_molecules,)),  
+            'order': Box(low=-float('inf'), high=float('inf'), shape=(10,))  
+        })
+
+        # Initialize the library and orders
+        self.library = []
+        self.orders = []
+
+        # Initialize the action mask
+        self.action_mask = np.zeros(self.max_molecules, dtype=bool)
 
     def step(self, action):
+        # If the action includes a design, update the library and the action mask
+        if 'design' in action:
+            new_molecules = self.design_library(action['design'])
+            self.library.extend(new_molecules)
+            self.action_mask[len(self.library):] = True
+
+        # If the action includes an order, perform the order
+        if 'order' in action:
+            molecule_index = action['order']['molecule']
+            if not self.action_mask[molecule_index]:
+                raise ValueError(f"The action for molecule {molecule_index} is masked.")
+            self.orders.append(self.perform_order(action['order']))
+
+        # Calculate the reward and check if the episode is done
+        reward = self.get_reward()
+        done = self.check_done()
+
+        return self.get_observation(), reward, done, {}
+
+    def reset(self):
+        self.library = None
+        self.orders = []
+        self.current_stage = 'design'
+        return self.get_observation()
+
+    def design_library(self, action):
+        # Implement the logic for designing the library based on the action
         ...
-        return observation, reward, terminated, truncated, info
 
-    def reset(self, seed=None, options=None):
-        ...
-        return observation, info
-
-    def render(self):
+    def select_order(self, action):
+        # Implement the logic for selecting an order based on the action
         ...
 
-    def close(self):
+    def get_observation(self):
+        # Implement the logic for generating the observation based on the current state
         ...
 
+    def get_reward(self):
+        # Implement the logic for calculating the reward based on the current state
+        ...
 
-# class DrugEnv(gym.Env):
-#     """Environment for Drug Discovery
-    
-#     Class that implements a gym.Env interface for drug discovery.
-#     See https://gymnasium.farama.org/ for details on gymnasium.
-    
-#     """
-#     def __init__(
-#         self,
-#     ) -> None:
+    def check_done(self):
+        # Implement the logic for checking if the episode is done
+        ...
 
-#         # spaces
-#         self.action_space = gym.spaces.Discrete(len(Actions))
-#         self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=self.shape, dtype=np.float64)
-
-#     def reset(self):
-#         self._done = False
-#         self.history = {}
-#         return self._get_observation()
-    
-#     def step(self, action: Iterable[rdkit.Chem.Mol]):
-#         self._done = False
-#         self._current_tick += 1
-
-#         if self._current_tick == self._end_tick:
-#             self._done = True
-
-#         step_reward = self._calculate_reward(action)
-#         self._total_reward += step_reward
-
-#         self._update_profit(action)
-
-#         trade = False
-#         if ((action == Actions.Buy.value and self._position == Positions.Short) or
-#             (action == Actions.Sell.value and self._position == Positions.Long)):
-#             trade = True
-
-#         if trade:
-#             self._position = self._position.opposite()
-#             self._last_trade_tick = self._current_tick
-
-#         self._position_history.append(self._position)
-#         observation = self._get_observation()
-#         info = dict(
-#             total_reward = self._total_reward,
-#             total_profit = self._total_profit,
-#             position = self._position.value
-#         )
-#         self._update_history(info)
-
-#         return observation, step_reward, self._done, info
-
-    
-# 	def _get_observation(self):
-#         return self.signal_features[(self._current_tick-self.window_size+1):self._current_tick+1]
-
-
-#     def _update_history(self, info):
-#         if not self.history:
-#             self.history = {key: [] for key in info.keys()}
-
-#         for key, value in info.items():
-#             self.history[key].append(value)
-
-
-#     def _process_data(self):
-#         raise NotImplementedError
-
-
-#     def _calculate_reward(self, action):
-#         raise NotImplementedError
-
-
-#     def _update_profit(self, action):
-#         raise NotImplementedError
+    def is_library_exhausted(self):
+        # Implement the logic for checking if the library is exhausted
+        ...
