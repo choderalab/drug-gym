376458f52dcd2c7e73caaf637f0fc689ecd5a324
Michael Retchin <michael.retchin@choderalab.org>
2023-08-07 13:33:52 -0400

    Added RDKit descriptors as an oracle function.

--- dgym/envs/oracle.py
+++ dgym/envs/oracle.py
@@ -1,15 +1,33 @@
 import dgl
 import torch
 import dgllife
-from dgym.collection import Collection
+from rdkit.Chem import Descriptors
+from dgym.collection import Collection, MoleculeCollection
 
 class Oracle:
     
     def __init__(self) -> None:
         self.cache = {}
 
+    def __call__(self, molecules: Collection):
+        
+        # identify uncached molecules
+        in_cache = lambda m: m.smiles not in self.cache
+        
+        if uncached_molecules := molecules.filter(in_cache):
+            
+            # make predictions
+            preds = self.predict(uncached_molecules)
+            
+            # cache results
+            self.cache.update(zip(uncached_molecules.smiles, preds))
+
+        # fetch all results (old and new) from cache
+        return [self.cache[m.smiles] for m in molecules]
+
     def predict(self, molecules: Collection):
         raise NotImplementedError
+
 
 class DGLOracle(Oracle):
 
@@ -31,25 +49,34 @@ class DGLOracle(Oracle):
         self.model = self.model.to(self.device)
         self.model.eval()
         
-    def predict(self, molecules: Collection):
+    def predict(self, molecules: MoleculeCollection):
         
-        # identify uncached molecules
-        in_cache = lambda m: m.smiles not in self.cache
-        if uncached_molecules := molecules.filter(in_cache):
+        # featurize
+        graphs = [
+            self.mol_to_graph(m.update_cache().mol)
+            for m in molecules
+        ]
+        graph_batch = dgl.batch(graphs).to(self.device)
+        feats_batch = graph_batch.ndata['h']
+        
+        # perform inference
+        preds = self.model(graph_batch, feats_batch).flatten().tolist()
+        
+        return preds
 
-            # featurize
-            graphs = [
-                self.mol_to_graph(m.update_cache().mol)
-                for m in uncached_molecules
-            ]
-            graph_batch = dgl.batch(graphs).to(self.device)
-            feats_batch = graph_batch.ndata['h']
-            
-            # perform inference
-            preds = self.model(graph_batch, feats_batch).flatten().tolist()
 
-            # cache results
-            self.cache.update(zip(uncached_molecules.smiles, preds))
 
-        # fetch all results (old and new) from cache
-        return [self.cache[m.smiles] for m in molecules]
+class RDKitOracle(Oracle):
+
+    def __init__(
+        self,
+        descriptor_name: str,
+    ):
+        super().__init__()
+        self.descriptor_name = descriptor_name
+
+        # load descriptor
+        self.descriptor = getattr(Descriptors, self.descriptor_name)
+
+    def predict(self, molecules: MoleculeCollection):
+        return [self.descriptor(m.mol) for m in molecules]
