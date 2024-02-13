63b4dff4edc04b76dc9af166d7748dd5556c8267
Michael Retchin <michael.retchin@choderalab.org>
2024-02-13 07:14:08 -0500

    Fixed design.

--- dgym/utils.py
+++ dgym/utils.py
@@ -19,93 +19,25 @@ import chemfp
 chemfp.set_license('20231114-columbia.edu@DAAABLGMDNEEHFALIFOLIONPFHFDJDOLHABF')
 
 
-# Sort fingerprints according to building blocks.
-# ---------------------------------
-
-def sort_fingerprints(fps, building_blocks):
-    """
-    Align building block library to pre-computed fingerprints.
-    
-    """
-    # get the argsort for the fingerprints
-    sorted_indices = [
-        fps.get_index_by_id(e.GetProp('ID'))
-        if e is not None else 0
-        for e in building_blocks
-    ]
-
-    # use argsort to reorder fingerprints
-    return fps.copy(
-        indices=sorted_indices,
-        reorder=False
-    )
-
-
-# Organize building blocks by functional class.
+# Building block processing.
 # -----------------------------------------------
+def compute_fingerprints(sdf_path: str = './', out_path: str = './out'):
+    
+    path = "./Enamine_Building_Blocks_Stock_262336cmpd_20230630.sdf"
 
-def partition_building_blocks(
-    building_blocks=None,
-    templates=None,
-    out_dir='.'
-):
-    """
-    Get partition building blocks according to provided functional groups.
+    # define fingerprint encoder
+    fp_type = chemfp.get_fingerprint_type("OpenBabel-ECFP4/1", {"nBits": 1024})
+    fp_iterator = fp_type.read_molecule_fingerprints(path, id_tag='ID')
 
-    """    
-    path = f'{out_dir}/out/building_block_subsets.pickle'
-    if os.path.exists(path):
-        with open(path, 'rb') as handle:
-            building_blocks = pickle.load(handle)
-    else:
-        # compute assignments
-        building_blocks = _partition_building_blocks(building_blocks, templates)
+    # open output file
+    with chemfp.open_fingerprint_writer(
+        './Enamine_Building_Blocks_Stock_262336cmpd_20230630.fpb',
+        metadata=fp_type.get_metadata(),
+        reorder=False
+    ) as writer:
         
-        # write to disk
-        with open(path, 'wb') as handle:
-            pickle.dump(building_blocks, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-    return building_blocks
-
-
-def _partition_building_blocks(building_blocks, templates):
-    """
-    Organize building block subsets by functional class.
-    
-    """
-    partitions = defaultdict(list)
-    
-    # for each building block
-    for idx, bb in enumerate(building_blocks):
-
-        # weirdly, some library entries are empty
-        if bb is None:
-            continue
-
-        # check substructure match with templates
-        for template in templates:
-            if bb.HasSubstructMatch(template):
-                partitions[template.GetProp('class')].append(
-                    {'index': idx, 'rdMol': bb}
-                )
-
-    # convert records to dataframes
-    partitions = {k: pd.DataFrame(v) for k, v in partitions.items()}
-    
-    return partitions
-
-
-def get_unique_reactants(reactions):
-    """
-    Get all unique template classes from provided reactions.
-    
-    """
-    unique_dict = {r.GetProp('class'): r
-                   for reaction in reactions
-                   for r in reaction.reactants}
-    unique_classes = list(unique_dict.values())
-    return unique_classes
-
+        for idx, (id, fp) in enumerate(tqdm(fp_iterator)):
+            writer.write_fingerprint(id, fp)
 
 
 # Plotting.
