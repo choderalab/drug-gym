f2e6a1ce0a0e05769a472f21c23042155f00c20b
Michael Retchin <michael.retchin@choderalab.org>
2024-04-07 16:22:10 -0400

    Experimenting with new utility function to facilitate MPO.

--- dgym/envs/utility.py
+++ dgym/envs/utility.py
@@ -7,8 +7,12 @@ from typing import Optional, Callable, Iterable
 
 class UtilityFunction:
     
-    def __init__(self, oracle, ideal, acceptable):
-
+    def __init__(
+        self,
+        oracle: Optional[Oracle] = None,
+        ideal: Iterable = [],
+        acceptable: Iterable = []
+    ):
         self.oracle = oracle
         self.ideal = np.array(ideal)
         self.acceptable = np.array(acceptable)
@@ -36,7 +40,12 @@ class UtilityFunction:
 
 class ClassicUtilityFunction(UtilityFunction):
     
-    def __init__(self, oracle, ideal, acceptable):
+    def __init__(
+        self,
+        oracle: Optional[Oracle] = None,
+        ideal: Iterable = [],
+        acceptable: Iterable = []
+    ):
         super().__init__(oracle, ideal, acceptable)
  
         self._lower_slope = self._slope(self.acceptable[0], 0.5, self.ideal[0], 1)
@@ -66,8 +75,55 @@ class ClassicUtilityFunction(UtilityFunction):
 
     @staticmethod
     def _slope(x1, y1, x2, y2):
-        return (y2 - y1) / (x2 - x1) if (x2 - x1) != 0 else 0
+        if (x2 - x1) == 0:
+            return 0
+        return (y2 - y1) / (x2 - x1)
 
     @staticmethod
     def _logistic(x, scale, midpoint):
         return 1 / (1 + np.exp((midpoint - x) * scale))
+
+
+class NewUtilityFunction(UtilityFunction):
+    
+    def __init__(
+        self,
+        oracle: Optional[Oracle] = None,
+        ideal: Iterable = [],
+        acceptable: Iterable = []
+    ):
+        super().__init__(oracle, ideal, acceptable)
+ 
+        self._lower_slope = self._slope(self.acceptable[0], 0.0, self.ideal[0], 1)
+        self._upper_slope = self._slope(self.ideal[1], 1, self.acceptable[1], 0.0)
+
+    def score_acceptable(self, value):
+        value = np.asarray(value)
+        res = np.empty_like(value, dtype=float)
+        
+        # Value less than lower acceptable limit (exponential decay)
+        mask = value < self.acceptable[0]
+        res[mask] = -np.exp(-(value[mask] - self.acceptable[0]))
+        
+        # Value between lower acceptable limit and lower ideal limit (linear interpolation)
+        mask = (self.acceptable[0] <= value) & (value <= self.ideal[0])
+        res[mask] = (value[mask] - self.acceptable[0]) * self._lower_slope
+        
+        # Value inside ideal range
+        mask = (self.ideal[0] < value) & (value < self.ideal[1])
+        res[mask] = 1
+        
+        # Value between upper ideal limit and upper acceptable limit (linear interpolation)
+        mask = (self.ideal[1] <= value) & (value <= self.acceptable[1])
+        res[mask] = (value[mask] - self.ideal[1]) * self._upper_slope + 1
+        
+        # Value greater than upper acceptable limit (exponential decay)
+        mask = value > self.acceptable[1]
+        res[mask] = -np.exp(value[mask] - self.acceptable[1])
+        
+        return res
+
+    @staticmethod
+    def _slope(x1, y1, x2, y2):
+        if (x2 - x1) == 0: return 0
+        return (y2 - y1) / (x2 - x1)
