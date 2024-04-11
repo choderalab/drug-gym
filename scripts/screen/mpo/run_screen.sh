14b036bf43abb8615c50e67d9332b0a38c9c31be
Michael Retchin <michael.retchin@choderalab.org>
2024-04-11 02:11:19 -0400

    Updated MPO screen so that it doesn't refer to a variable that doesn't exist.

--- scripts/screen/mpo/run_screen.sh
+++ scripts/screen/mpo/run_screen.sh
@@ -23,16 +23,12 @@ NUM_TRIALS=100
 # Run multiple trials for each scorer
 for (( TRIAL=1; TRIAL<=NUM_TRIALS; TRIAL++ )); do
 
-    # Generate noise levels from 0 to 2 with a step of 0.1
-    for TARGET in $(seq $START $INCREMENT $END); do
-        echo "Trial $TRIAL"
-        
-        # Submit the job with bsub
-        bsub -q gpuqueue -n 2 -gpu "num=1:j_exclusive=yes" -R "rusage[mem=8] span[hosts=1]" -W 0:10 \
-                -o "${LOGS_DIR}/mpo_trial_${TRIAL}.stdout" \
-                -eo "${LOGS_DIR}/mpo_trial_${TRIAL}.stderr" \
-                python3 "$PYTHON_SCRIPT" --out_dir "$RUN_DIR"
-    done
+    echo "Trial $TRIAL"
     
-    echo "Completed all trials"
+    # Submit the job with bsub
+    bsub -q gpuqueue -n 2 -gpu "num=1:j_exclusive=yes" -R "rusage[mem=8] span[hosts=1]" -W 0:10 \
+            -o "${LOGS_DIR}/mpo_trial_${TRIAL}.stdout" \
+            -eo "${LOGS_DIR}/mpo_trial_${TRIAL}.stderr" \
+            python3 "$PYTHON_SCRIPT" --out_dir "$RUN_DIR"
 done
+echo "Completed all trials"
