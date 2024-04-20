e6d5f1fe74ea235e5fc0941c9dfb08f45296efc9
Michael Retchin <michael.retchin@choderalab.org>
2024-04-20 12:20:22 -0400

    Updated temperature script.

--- scripts/selection/temperature/run_selection_temperature.sh
+++ scripts/selection/temperature/run_selection_temperature.sh
@@ -17,29 +17,28 @@ LOGS_DIR="${RUN_DIR}/logs"
 mkdir -p "$RUN_DIR"
 mkdir -p "$LOGS_DIR"
 
-# Define start, end, and increment for temperature levels
+# Define start, end, and increment for noise levels
 START=0
-END=20
+END=10  # For noise levels up to 2
 INCREMENT=1
 
-# Number of trials to run for each level
-NUM_TRIALS=50
+# Number of trials to run for each noise level
+NUM_TRIALS=200
 
-# Generate temperature levels from START to END with a step of INCREMENT
-for TEMP_INT in $(seq $START $INCREMENT $END); do
-    TEMP=$(echo "scale=2; $TEMP_INT / 20" | bc)
-    echo "Running trials for temperature level: $TEMP"
+# Run multiple trials for this noise level
+for (( TRIAL=1; TRIAL<=NUM_TRIALS; TRIAL++ )); do
 
-    # Run multiple trials for this temperature level
-    for (( TRIAL=1; TRIAL<=NUM_TRIALS; TRIAL++ )); do
+    # Generate noise levels from 0 to 2 with a step of 0.1
+    for TEMP_INT in $(seq $START $INCREMENT $END); do
+        TEMP=$(echo "scale=2; $TEMP_INT / 10" | bc)
         echo "Trial $TRIAL for temperature $TEMP"
-
+        
         # Submit the job with bsub
-        bsub -q gpuqueue -n 2 -gpu "num=1:j_exclusive=yes" -R "rusage[mem=8] span[hosts=1]" -W 0:05 \
+        bsub -q gpuqueue -n 2 -gpu "num=1:j_exclusive=yes" -R "rusage[mem=8] span[hosts=1]" -W 1:30 \
              -o "${LOGS_DIR}/temp_${TEMP}_trial_${TRIAL}.stdout" \
              -eo "${LOGS_DIR}/temp_${TEMP}_trial_${TRIAL}.stderr" \
              python3 "$PYTHON_SCRIPT" --temperature "$TEMP" --out_dir "$RUN_DIR"
     done
-
     echo "Completed all trials for temperature level: $TEMP"
 done
+echo "All trials completed."
--- scripts/selection/temperature/selection_temperature.py
+++ scripts/selection/temperature/selection_temperature.py
@@ -1,111 +1,232 @@
+import uuid
 import argparse
 import dgym as dg
-
-# load all data
-path = '../../../../dgym-data'
-
-deck = dg.MoleculeCollection.load(
-    f'{path}/DSi-Poised_Library_annotated.sdf',
-    reactant_names=['reagsmi1', 'reagsmi2', 'reagsmi3']
-)
-
-reactions = dg.ReactionCollection.from_json(
-    path = f'{path}/All_Rxns_rxn_library.json',
-    smarts_col = 'reaction_string',
-    classes_col = 'functional_groups'
-)
-
-building_blocks = dg.datasets.disk_loader(f'{path}/Enamine_Building_Blocks_Stock_262336cmpd_20230630.sdf')
-fingerprints = dg.datasets.fingerprints(f'{path}/Enamine_Building_Blocks_Stock_262336cmpd_20230630_atoms.fpb')
-
-import torch
-import pyarrow.parquet as pq
-table = pq.read_table(f'{path}/sizes.parquet')[0]
-sizes = torch.tensor(table.to_numpy())
-
-# Docking oracles
-from dgym.envs.oracle import DockingOracle
-from dgym.envs.utility import ClassicUtilityFunction
-
-config = {
-    'center_x': 44.294,
-    'center_y': 28.123,
-    'center_z': 2.617,
-    'size_x': 22.5,
-    'size_y': 22.5,
-    'size_z': 22.5,
-    'search_mode': 'balanced',
-    'scoring': 'gnina',
-    'seed': 5
-}
-
-# Create noiseless evaluators
-docking_oracle = DockingOracle(
-    'ADAM17 affinity',
-    receptor_path=f'{path}/ADAM17.pdbqt',
-    config=config
-)
-
-docking_utility = ClassicUtilityFunction(
-    docking_oracle,
-    ideal=(8.5, 9.5),
-    acceptable=(7.125, 9.5)
-)
-
 import pandas as pd
-from dgym.molecule import Molecule
-from dgym.envs.designer import Designer, Generator
-from dgym.envs.drug_env import DrugEnv
-from dgym.agents import SequentialDrugAgent
-from dgym.agents.exploration import EpsilonGreedy
-from dgym.experiment import Experiment
 
-designer = Designer(
-    Generator(building_blocks, fingerprints, sizes),
-    reactions,
-    cache = True
-)
+def get_data(path):
 
-initial_library = dg.MoleculeCollection([deck[659]])
-initial_library.update_annotations()
+    deck = dg.MoleculeCollection.load(
+        f'{path}/DSi-Poised_Library_annotated.sdf',
+        reactant_names=['reagsmi1', 'reagsmi2', 'reagsmi3']
+    )
 
-drug_env = DrugEnv(
-    designer,
-    library = initial_library,
-    assays = [docking_oracle],
-    budget = 500,
-    utility_function = docking_utility,
-)
+    reactions = dg.ReactionCollection.from_json(
+        path = f'{path}/All_Rxns_rxn_library.json',
+        smarts_col = 'reaction_string',
+        classes_col = 'functional_groups'
+    )
+
+    building_blocks = dg.datasets.disk_loader(
+        f'{path}/Enamine_Building_Blocks_Stock_262336cmpd_20230630.sdf')
+    fingerprints = dg.datasets.fingerprints(
+        f'{path}/Enamine_Building_Blocks_Stock_262336cmpd_20230630_atoms.fpb')
+
+    import torch
+    import pyarrow.parquet as pq
+    table = pq.read_table(f'{path}/sizes.parquet')[0]
+    sizes = torch.tensor(table.to_numpy())
+
+    return deck, reactions, building_blocks, fingerprints, sizes
+
+def get_initial_library(deck, designer):
+    
+    # select first molecule
+    import random
+    def _select_molecule(deck):
+        initial_index = random.randint(0, len(deck) - 1)
+        initial_molecule = deck[initial_index]
+        if len(initial_molecule.reactants) == 2 \
+            and designer.match_reactions(initial_molecule):
+            return initial_molecule
+        else:
+            return _select_molecule(deck)
+
+    initial_molecules = [_select_molecule(deck) for _ in range(5)]
+    library = dg.MoleculeCollection(initial_molecules).update_annotations()
+    
+    return library
+    
+def get_docking_config(path: str, target_index: int):
+    
+    import os
+
+    dockstring_dir = f'{path}/dockstring_targets/'
+    files = os.listdir(dockstring_dir)
+    configs = sorted([f for f in files if 'conf' in f])
+    targets = sorted([f for f in files if 'target' in f])
+
+    with open(dockstring_dir + configs[target_index], 'r') as f:
+        config_ = f.readlines()
+        config_ = [c.replace('\n', '') for c in config_]
+        config_ = [c.split(' = ') for c in config_ if c]
+        config_ = {c[0]: float(c[1]) for c in config_}
+
+    target_file = targets[target_index]
+    target = target_file.split('_')[0]
+    
+    config = {
+        'search_mode': 'detailed',
+        'scoring': 'vina',
+        'seed': 5,
+        'size_x': 22.5,
+        'size_y': 22.5,
+        'size_z': 22.5,
+        **config_
+    }
+    
+    return target, config
+
+def get_oracles(path: str, target_index: int):
+
+    from dgym.envs.oracle import \
+        DockingOracle, CatBoostOracle, RDKitOracle, NoisyOracle
+    from dgym.envs.utility import ClassicUtilityFunction
+    
+    target, config = get_docking_config(path, target_index)
+
+    pIC50_oracle = DockingOracle(
+        f'{target} pIC50',
+        receptor_path=f'{path}/dockstring_targets/{target}_target.pdbqt',
+        config=config
+    )
+    log_P_oracle = RDKitOracle('Log P', descriptor='MolLogP')
+    log_S_oracle = CatBoostOracle(
+        'Log S', path='../../../dgym/envs/models/aqsolcb.model')
+    
+    return pIC50_oracle, log_P_oracle, log_S_oracle
+
+def get_multiple_utility_functions(
+    pIC50_oracle,
+    log_P_oracle,
+    log_S_oracle,
+    sigma=1.0
+):
+    from dgym.envs.utility import (
+        ClassicUtilityFunction, MultipleUtilityFunction
+    )
+
+    # Define utility functions
+    pIC50_utility = ClassicUtilityFunction(
+        pIC50_oracle, ideal=(9.5, 13), acceptable=(8, 13))
+    log_P_utility = ClassicUtilityFunction(
+        log_P_oracle, ideal=(0.5, 1.85), acceptable=(-0.5, 3.5))
+    log_S_utility = ClassicUtilityFunction(
+        log_S_oracle, ideal=(-3, 1), acceptable=(-4, 1))
+
+    # Assemble assays and surrogate models
+    assays = [
+        pIC50_oracle,
+        log_P_oracle,
+        log_S_oracle,
+        pIC50_oracle.surrogate(sigma=sigma),
+        log_P_oracle.surrogate(sigma=sigma),
+        log_S_oracle.surrogate(sigma=sigma),
+    ]
+
+    # Environment tolerates acceptable ADMET
+    from copy import deepcopy
+    utility_agent = MultipleUtilityFunction(
+        utility_functions = [pIC50_utility, log_P_utility, log_S_utility],
+        weights = [0.8, 0.1, 0.1]
+    )
+    utility_env = deepcopy(utility_agent)
+    utility_env.utility_functions[1].ideal = utility_env.utility_functions[1].acceptable
+    utility_env.utility_functions[2].ideal = utility_env.utility_functions[2].acceptable
+    
+    return assays, utility_agent, utility_env
+
+def get_agent_sequence(temperature: float):
+    """
+    Make the sequence for the DrugAgent.
+    """
+
+    design_grow = {'name': 'design', 'batch_size': 5, 'parameters': {'strategy': 'grow', 'size': 8}}
+    design_replace = {'name': 'design', 'batch_size': 5, 'parameters': {'strategy': 'replace', 'size': 8, 'temperature': temperature}}
+    score = {'name': ['Noisy ABL1 pIC50', 'Noisy Log S', 'Noisy Log P'], 'batch_size': 40}
+    make = {'name': 'make', 'batch_size': 8}
+    test = {'name': ['ABL1 pIC50', 'Log S', 'Log P'], 'batch_size': 8} # 8
+    design_and_score = [design_replace, score]
+
+    return [*(design_and_score * 1), design_grow, score, make, test]
 
 # Parse command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--temperature", type=float, help="Creativity of the molecular designs")
-parser.add_argument("--out_dir", type=str, help="Where to put the resulting JSONs")
-
+parser.add_argument(
+    "--out_dir", type=str, help="Where to put the resulting JSONs")
+parser.add_argument(
+    "--temperature", type=float, help="Boltzmann temperature for building block sampling")
 args = parser.parse_args()
 
-# Run the experiment
-sequence = [
-    {'name': 'ideate', 'parameters': {'temperature': args.temperature, 'size': 10, 'strict': False}},
-    {'name': 'ideate', 'parameters': {'temperature': args.temperature, 'size': 10, 'strict': True}},
-    {'name': 'ADAM17 affinity'},
-]
+# Run experiment
+path = '../../../../dgym-data'
 
-drug_agent = SequentialDrugAgent(
-    sequence = sequence,
-    utility_function = docking_utility,
-    exploration_strategy = EpsilonGreedy(epsilon = 0.0),
-    branch_factor = 2
+# Load all data
+(
+    deck,
+    reactions,
+    building_blocks,
+    fingerprints,
+    sizes
+) = get_data(path)
+
+# Get starting library
+from dgym.envs.designer import Designer, Generator
+designer = Designer(
+    Generator(building_blocks, fingerprints, sizes), reactions, cache = True)
+library = get_initial_library(deck, designer)
+
+# Get Oracles
+(
+    pIC50_oracle,
+    log_P_oracle,
+    log_S_oracle
+) = get_oracles(
+    path=path,
+    target_index=0
 )
 
-experiment = Experiment(drug_agent, drug_env)
-result = experiment.run(**vars(args))
+# Create multiple utility functions
+(
+    assays,
+    utility_agent,
+    utility_env
+) = get_multiple_utility_functions(
+    pIC50_oracle,
+    log_P_oracle,
+    log_S_oracle,
+    sigma = args.sigma
+)
+
+# Create DrugEnv
+from dgym.envs import DrugEnv
+drug_env = DrugEnv(
+    designer = designer,
+    library = library,
+    assays = assays,
+    utility_function = utility_env
+)
+
+# Create DrugAgent
+from dgym.agents import SequentialDrugAgent
+from dgym.agents.exploration import EpsilonGreedy
+sequence = get_agent_sequence(temperature = args.temperature)
+drug_agent = SequentialDrugAgent(
+    sequence = sequence,
+    exploration_strategy = EpsilonGreedy(epsilon=0.2),
+    utility_function = utility_agent
+)
+
+# Create and run Experiment
+from dgym.experiment import Experiment
+experiment = Experiment(
+    drug_agent=drug_agent, drug_env=drug_env)
+file_path = f'{args.out_dir}/selection_temperature_{args.sigma}_{uuid.uuid4()}.json'
+result = experiment.run(**vars(args), out=file_path)[0]
 
 # Export results
 import json
 import uuid
 from utils import serialize_with_class_names
 
-file_path = f'{args.out_dir}/selection_temperature_{uuid.uuid4()}.json'
 result_serialized = serialize_with_class_names(result)
 json.dump(result_serialized, open(file_path, 'w'))
