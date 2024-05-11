6b96d94872dc9a06692274f4a685625e40b3da9d
Michael Retchin <michael.retchin@choderalab.org>
2024-05-10 21:15:16 -0400

    Hopefully running the GPU in shared mode helps.

--- scripts/selection/noise/run_selection_noise.sh
+++ scripts/selection/noise/run_selection_noise.sh
@@ -34,7 +34,7 @@ for (( TRIAL=1; TRIAL<=NUM_TRIALS; TRIAL++ )); do
         echo "Trial $TRIAL for noise $NOISE"
 
         # Submit a bsub job to run the script in parallel instances
-        bsub -q gpuqueue -n 4 -gpu "num=1" -R "rusage[mem=8] span[hosts=1]" -W 5:59 \
+        bsub -q gpuqueue -n 4 -gpu "num=1:j_exclusive=yes:mode=shared" -R "rusage[mem=8] span[hosts=1]" -W 5:59 \
              -o "${LOGS_DIR}/temp_${NOISE}_trial_${TRIAL}.stdout" \
              -eo "${LOGS_DIR}/temp_${NOISE}_trial_${TRIAL}.stderr" \
              "for i in $(seq -s ' ' 1 $NUM_PARALLEL); do python3 '$PYTHON_SCRIPT' --sigma $NOISE --out_dir '$RUN_DIR' & done; wait"
