25521650ddff7c18cc207359840add451563c1cd
Michael Retchin <michael.retchin@choderalab.org>
2024-05-11 19:04:03 -0400

    Completed bash script for continuing experiments.

---
+++ scripts/selection/noise/run_selection_noise_continued.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Disable core dumps
+ulimit -c 0
+
+# Define the path to your Python script
+PYTHON_SCRIPT="./selection_noise.py"
+
+# Define output directory for results
+RUN_DIR="/data/chodera/retchinm/noise/2024-05-10_23-36-56"
+LOGS_DIR="${RUN_DIR}/logs"
+
+# Make sure the new directories exist
+mkdir -p "$RUN_DIR"
+mkdir -p "$LOGS_DIR"
+
+# Loop over each JSON file in the directory
+for EXPERIMENT_STATE in "$RUN_DIR"/*.json; do
+    # Submit a bsub job to run the script in parallel instances
+    bsub -q gpuqueue -n 4 -gpu "num=1:j_exclusive=yes:mode=shared" -R "rusage[mem=8] span[hosts=1]" -W 5:59 \
+            -o "${LOGS_DIR}/${EXPERIMENT_STATE}.stdout" \
+            -eo "${LOGS_DIR}/${EXPERIMENT_STATE}.stderr" \
+            python3 '$PYTHON_SCRIPT' --experiment_state_path '$EXPERIMENT_STATE' --out_dir '$RUN_DIR'
+    echo "Submitted experiment: $EXPERIMENT_STATE"
+done
+echo "All trials completed."
