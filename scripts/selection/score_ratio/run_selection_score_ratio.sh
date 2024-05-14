2754d7cb97f1f345eedb7a11742ce2127effa178
Michael Retchin <michael.retchin@choderalab.org>
2024-05-14 00:39:37 -0400

    Using exclusive GPUs to maybe avoid core dumps?

--- scripts/selection/score_ratio/run_selection_score_ratio.sh
+++ scripts/selection/score_ratio/run_selection_score_ratio.sh
@@ -33,7 +33,7 @@ for (( TRIAL=1; TRIAL<=NUM_TRIALS; TRIAL++ )); do
         echo "Trial $TRIAL for score ratio $SCORE_RATIO"
 
         # Submit a bsub job to run the script in parallel instances
-        bsub -q gpuqueue -n 4 -gpu "num=1" -R "rusage[mem=8G] span[hosts=1]" -W 5:59 \
+        bsub -q gpuqueue -n 4 -gpu "num=1:j_exclusive=yes:mode=shared" -R "rusage[mem=8G] span[hosts=1]" -W 5:59 \
              -o "${LOGS_DIR}/temp_${SCORE_RATIO}_trial_${TRIAL}.stdout" \
              -eo "${LOGS_DIR}/temp_${SCORE_RATIO}_trial_${TRIAL}.stderr" \
              python3 $PYTHON_SCRIPT --score_ratio $SCORE_RATIO --out_dir "$RUN_DIR"
